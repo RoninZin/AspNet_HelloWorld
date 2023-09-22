c7f6837fde660c0df7bd9152842a7c0fa54e15aa
Roniberto Rodrigues Alves <ronirodrigues123@hotmail.com>
2023-09-22 08:58:18 -0300

    Aula 1 de AspNET com 3 endpoints

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+
+    [Route("~/")]
+    [ApiController]
+    public class HelloController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello World!";
+        }
+
+        [HttpGet("/bsmsgeneration")]
+        public string BsmGeneration()
+        {
+            return "BSM’s da Generation Brasil:\nPersistência\nMentalidade de Crescimento\nResponsabilidade pessoal\nOrientação ao Futuro\nComunicação\nProatividade\nOrientação ao Detalhe\nTrabalho em Equipe";
+        }
+
+        [HttpGet("/aprendizagem")]
+        public string AprendizagemSemana()
+        {
+            return "O objetivo de aprender as funcionalidades do aspnet\nAplicar as funcionalidadesg\nAprender sobre a edição do meu GitHub e Linkedin ";
+        }
+    }
+}
